70a2a8e50003906e1594e62918a9c4a49ce36f9a
Francesco <francesco.scandurra11@gmail.com>
2026-05-26 20:28:36 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "radar-of-everything-v1";
+const URLS_TO_CACHE = [
+  "/radar/",
+  "/radar/index.html",
+  "/radar/manifest.json",
+  "/radar/icons/icon-192.png",
+  "/radar/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
